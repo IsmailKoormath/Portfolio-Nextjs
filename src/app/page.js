a8890319735ce13094de18d216e2acccc7083543
Ismail K <ismailkoormath037@gmail.com>
2023-05-01 17:38:53 +0530

    first page desinged

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,14 @@
 import { BsFillMoonStarsFill } from "react-icons/bs";
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillYoutube,
+} from "react-icons/ai";
+import Image from "next/image";
+import deved from "../../public/dev-ed-wave.png";
+import design from "../../public/design.png";
+import consulting from "../../public/consulting.png";
+import code from "../../public/code.png";
 export default function Home() {
   return (
     <main className="bg-white px-10">
@@ -11,7 +21,7 @@ export default function Home() {
             </li>
             <li>
               <a
-                className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+                className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-6"
                 href="#"
               >
                 Resume
@@ -19,11 +29,81 @@ export default function Home() {
             </li>
           </ul>
         </nav>
-        <div>
-          <h2>Ismail</h2>
-          <h3>Font-end Reactjs Developer</h3>
+        <div className="text-center p-10">
+          <h2
+            className="text-5xl py-2 text-teal-600 font-me
+          "
+          >
+            Ismail k
+          </h2>
+          <h3 className="text-2xl py-2">Font-end Reactjs Developer</h3>
+          <p className="text-md py-5 leading-8 text-gray-800">
+            Freelancer providing services for programming and design content
+            needs. Join me down below and let's get cracking
+          </p>
+        </div>
+        <div className="text-5xl flex justify-center gap-14 py-3 text-gray-600">
+          <AiFillTwitterCircle />
+          <AiFillLinkedin />
+          <AiFillYoutube />
+        </div>
+        <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
+          <Image src={deved} layout="fill" objectFit="cover" />
         </div>
       </section>
+      <section>
+        <div>
+          <h3 className="text-3xl py-1">Services I offer</h3>
+          <p className="text-md py-2 leading-8 text-gray-800">
+            Since the beginning of my journy as a freelance designer and
+            developer, I've done remote work for
+            <span className="text-teal-500"> agencies</span>
+            consulted for <span className="text-teal-500"> startups</span>
+            and collaborated with talented people to create digital products for
+            both business and consumer use.
+          </p>
+          <p className="text-md py-2 leading-8 text-gray-800">
+            I offer from a wide range of services,including brand
+            design,programming and teaching
+          </p>
+        </div>
+        <div>
+          <div className="text-center shadow-lg p-10 rounded-xl my-10">
+            <Image src={design} width={100} height={100} />
+            <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
+            <p className="py-2">
+              Creating elegant designs suited for your needs design theory.
+            </p>
+            <h4 className="py-4 text-teal-600">Design tools I use</h4>
+            <p className="text-gray-800 py-1">Photoshop</p>
+            <p className="text-gray-800 py-1">Illustrator</p>
+            <p className="text-gray-800 py-1">Figma</p>
+          </div>
+          <div className="text-center shadow-lg p-10 rounded-xl my-10">
+            <Image src={code} width={100} height={100} />
+            <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
+            <p className="py-2">
+              Creating elegant designs suited for your needs design theory.
+            </p>
+            <h4 className="py-4 text-teal-600">Design tools I use</h4>
+            <p className="text-gray-800 py-1">Photoshop</p>
+            <p className="text-gray-800 py-1">Illustrator</p>
+            <p className="text-gray-800 py-1">Figma</p>
+          </div>
+          <div className="text-center shadow-lg p-10 rounded-xl my-10">
+            <Image src={consulting} width={100} height={100} />
+            <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
+            <p className="py-2">
+              Creating elegant designs suited for your needs design theory.
+            </p>
+            <h4 className="py-4 text-teal-600">Design tools I use</h4>
+            <p className="text-gray-800 py-1">Photoshop</p>
+            <p className="text-gray-800 py-1">Illustrator</p>
+            <p className="text-gray-800 py-1">Figma</p>
+          </div>
+        </div>
+      </section>
+      <section><div><h3 className="text-3xl py-1">Portfolio</h3></div></section>
     </main>
   );
 }
