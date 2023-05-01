64b137e5595c1f84a91935b98cf9e4fbd42e6041
Ismail K <ismailkoormath037@gmail.com>
2023-05-01 20:03:33 +0530

    add portfolio images

--- src/app/page.js
+++ src/app/page.js
@@ -4,20 +4,36 @@ import {
   AiFillLinkedin,
   AiFillYoutube,
 } from "react-icons/ai";
+
 import Image from "next/image";
 import deved from "../../public/dev-ed-wave.png";
 import design from "../../public/design.png";
 import consulting from "../../public/consulting.png";
 import code from "../../public/code.png";
+import web1 from "../../public/web1.png";
+import web2 from "../../public/web2.png";
+import web3 from "../../public/web3.png";
+import web4 from "../../public/web4.png";
+import web5 from "../../public/web5.png";
+import web6 from "../../public/web6.png";
+
+
+
+
+
 export default function Home() {
+
+  // const [darkMode, setDarkMode] = useState(false)
+
   return (
-    <main className="bg-white px-10">
+    // <div className={darkMode ? "dark" :""}>
+    <main className= "bg-white px-10 md:px-20 lg:px-40 dark:bg-gr">
       <section className="min-h-screen">
         <nav className="py-10 mb-12 flex justify-between">
           <h1 className="text-xl font-burtons">developedbyed</h1>
           <ul className="flex items-center">
             <li>
-              <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
+              {/* <BsFillMoonStarsFill onClick={()=>setDarkMode(!darkMode)} className="cursor-pointer text-2xl" /> */}
             </li>
             <li>
               <a
@@ -30,14 +46,13 @@ export default function Home() {
           </ul>
         </nav>
         <div className="text-center p-10">
-          <h2
-            className="text-5xl py-2 text-teal-600 font-me
-          "
-          >
+          <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">
             Ismail k
           </h2>
-          <h3 className="text-2xl py-2">Font-end Reactjs Developer</h3>
-          <p className="text-md py-5 leading-8 text-gray-800">
+          <h3 className="text-2xl py-2 md:text-3xl">
+            Font-end Reactjs Developer
+          </h3>
+          <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto">
             Freelancer providing services for programming and design content
             needs. Join me down below and let's get cracking
           </p>
@@ -47,7 +62,7 @@ export default function Home() {
           <AiFillLinkedin />
           <AiFillYoutube />
         </div>
-        <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
+        <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
           <Image src={deved} layout="fill" objectFit="cover" />
         </div>
       </section>
@@ -67,7 +82,7 @@ export default function Home() {
             design,programming and teaching
           </p>
         </div>
-        <div>
+        <div className="lg:flex gap-10">
           <div className="text-center shadow-lg p-10 rounded-xl my-10">
             <Image src={design} width={100} height={100} />
             <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
@@ -103,7 +118,80 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section><div><h3 className="text-3xl py-1">Portfolio</h3></div></section>
+      <section>
+        <div>
+          <h3 className="text-3xl py-1">Portfolio</h3>
+          <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+            Since the beginning of my journey as a freelance designer and
+            developer, I've done remote work for
+            <span className="text-teal-500"> agencies </span>
+            consulted for <span className="text-teal-500">startups </span>
+            and collaborated with talanted people to create digital products for
+            both business and consumer use.
+          </p>
+          <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+            I offer from a wide range of services, including brand design,
+            programming and teaching.
+          </p>
+        </div>
+        <div className="flex flex-col gap-10 py-10 md:flex-row md:flex-wrap">
+          <div className="basis-1/3 flex-1">
+            <Image
+              src={web1}
+              className="rounded-lg object-cover"
+              width={"100%"}
+              height={"100%"}
+              layout="responsive"
+            />
+          </div>
+          <div className="basis-1/3 flex-1">
+            <Image
+              src={web2}
+              className="rounded-lg object-cover"
+              width={"100%"}
+              height={"100%"}
+              layout="responsive"
+            />
+          </div>
+          <div className="basis-1/3 flex-1">
+            <Image
+              src={web3}
+              className="rounded-lg object-cover"
+              width={"100%"}
+              height={"100%"}
+              layout="responsive"
+            />
+          </div>
+          <div className="basis-1/3 flex-1">
+            <Image
+              src={web4}
+              className="rounded-lg object-cover"
+              width={"100%"}
+              height={"100%"}
+              layout="responsive"
+            />
+          </div>
+          <div className="basis-1/3 flex-1">
+            <Image
+              src={web5}
+              className="rounded-lg object-cover"
+              width={"100%"}
+              height={"100%"}
+              layout="responsive"
+            />
+          </div>
+          <div className="basis-1/3 flex-1">
+            <Image
+              src={web6}
+              className="rounded-lg object-cover"
+              width={"100%"}
+              height={"100%"}
+              layout="responsive"
+            />
+          </div>
+        </div>
+      </section>
     </main>
+    // </div>
   );
 }
